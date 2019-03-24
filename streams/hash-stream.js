95cd22cd91789886c20686a2fa13a3a560f9bc10
Bar Cohen <barcohen10@gmail.com>
2019-03-24 20:42:16 +0200

    extract createHash function

--- streams/hash-stream.js
+++ streams/hash-stream.js
@@ -1,25 +1,17 @@
 const { Transform } = require('stream');
 const crypto = require('crypto');
 
-const createHash = Symbol()
-
 class HashStream {
     constructor() {
         this.hashedLines = {}
     }
 
-    [createHash](line) {
-        return crypto.createHash('md5')
-            .update(line)
-            .digest("hex")
-    }
-
     getTransform() {
-        const self = this;
+        const self = this
         return new Transform({
             transform(line, encoding, cb) {
                 let result = line
-                const hashedLine = self[createHash](line)
+                const hashedLine = crypto.createHash('md5').update(line.toString()).digest("hex")
 
                 if (!self.hashedLines[hashedLine]) {
                     self.hashedLines[hashedLine] = true
