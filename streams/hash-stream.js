30592ceb70d1377238b47989306ee2e895d65bfd
Bar Cohen <barcohen10@gmail.com>
2019-03-24 13:00:46 +0200

    remove reference to line type in hash-stream

--- streams/hash-stream.js
+++ streams/hash-stream.js
@@ -1,5 +1,4 @@
 const { Transform } = require('stream');
-const { LINE_TYPE } = require('../constants')
 const crypto = require('crypto');
 
 const createHash = Symbol()
@@ -11,27 +10,23 @@ class HashStream {
 
     [createHash](line) {
         return crypto.createHash('md5')
-        .update(line)
-        .digest("hex")
+            .update(line)
+            .digest("hex")
     }
 
     getTransform() {
         const self = this;
         return new Transform({
-            readableObjectMode: true,
-            writableObjectMode: true,
             transform(line, encoding, cb) {
                 let result = line
+                const hashedLine = self[createHash](line)
 
-                if (line.type === LINE_TYPE.REGULAR) {
-                    const hashedLine = self[createHash](line.value)
-        
-                    if (!self.hashedLines[hashedLine]) {
-                        self.hashedLines[hashedLine] = true
-                    } else {
-                        result = null
-                    }
+                if (!self.hashedLines[hashedLine]) {
+                    self.hashedLines[hashedLine] = true
+                } else {
+                    result = ''
                 }
+
                 this.push(result)
                 cb()
             }
