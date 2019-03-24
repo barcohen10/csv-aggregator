5e991c871c49e50b5dce5f716d60efa8f4333c92
Bar Cohen <barcohen10@gmail.com>
2019-03-24 21:34:16 +0200

    create csvRowToObject - from string to object to inject to db (with hashed id)

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const zlib = require('zlib');
+const crypto = require('crypto');
 const { LINE_TYPE, DATA_PATH } = require('../constants');
 const { createLineReaderStream } = require('../streams');
 
@@ -18,14 +19,27 @@ function getCompressedFiles(directoryPath) {
 function getCsvHeader(fileName) {
     return new Promise((resolve, reject) => {
         fs.createReadStream(`${DATA_PATH}/${fileName}`)
-        .pipe(zlib.createGunzip())
-        .pipe(createLineReaderStream(LINE_TYPE.HEADER))
-        .on('data', chunk => resolve(chunk.toString()))
-        .on('error', err => reject(err));
-    });
+            .pipe(zlib.createGunzip())
+            .pipe(createLineReaderStream(LINE_TYPE.HEADER))
+            .on('data', chunk => resolve(chunk.toString()))
+            .on('error', err => reject(err))
+    })
+}
+
+function csvRowToObject(keys, row) {
+    const obj = { id: crypto.createHash('md5').update(row).digest("hex") }
+    const rowArray = row.split(',')
+
+    for (let i = 0; i < rowArray.length; i++) {
+        if (keys[i]) {
+            obj[keys[i]] = rowArray[i]
+        }
+    }
+    return obj
 }
 
 module.exports = {
     getCompressedFiles,
     getCsvHeader,
+    csvRowToObject,
 }
