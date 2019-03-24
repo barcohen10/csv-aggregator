89cbed6c1882da01caf821946db1ad907b4efa06
Bar Cohen <barcohen10@gmail.com>
2019-03-24 10:27:42 +0200

    Create entry point for all streams

---
+++ streams/index.js
@@ -0,0 +1,9 @@
+const LineAnalyzerStream = require('./line-analyzer-stream');
+const HashStream = require('./hash-stream')
+const CSVStream = require('./csv-stream')
+
+module.exports = {
+    LineAnalyzerStream,
+    HashStream,
+    CSVStream,
+}
