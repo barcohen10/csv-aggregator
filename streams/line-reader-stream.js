2250019061f3ece839ac56e18b685362d65673e6
Bar Cohen <barcohen10@gmail.com>
2019-03-24 12:58:04 +0200

    Create createLineReaderStream - parse chunk to lines

---
+++ streams/line-reader-stream.js
@@ -0,0 +1,26 @@
+const { Transform } = require('stream');
+const { LINE_TYPE } = require('../constants')
+
+function createLineReaderStream(type = LINE_TYPE.BODY) {
+    return new Transform({
+        transform(chunk, encoding, cb) {
+            let lines = chunk
+                .toString()
+                .split(/\r\n/g)
+                .filter(line => !!line);
+
+            if (type === LINE_TYPE.HEADER) {
+                this.push(lines[0]);
+            } else {
+                const bodyLines = lines.slice(1);
+                while (bodyLines.length > 0) {
+                    this.push(bodyLines.shift());
+                }
+            }
+
+            cb();
+        },
+    });
+}
+
+module.exports = createLineReaderStream;
