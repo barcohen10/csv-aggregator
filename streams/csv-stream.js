2d4882bb057811e555a24b91e42e7b61a87ffb23
Bar Cohen <barcohen10@gmail.com>
2019-03-24 13:02:47 +0200

    pretty and re-organized

--- streams/csv-stream.js
+++ streams/csv-stream.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const zlib = require('zlib');
 const { Transform, PassThrough } = require('stream');
-const { LINE_TYPE, MAX_CSV_LENGTH, WRITE_PATH, AGGREGATION_FINISHED } = require('../constants')
+const { MAX_CSV_LENGTH, WRITE_PATH, AGGREGATION_FINISHED } = require('../constants')
 const eventEmitter = require('../emitter')
 
 const setHeader = Symbol()
@@ -12,11 +12,10 @@ const writeFileToDisc = Symbol()
 
 class CSVStream {
 
-    constructor() {
-        this.file = ''
-        this.filesCount = 1
-        this.header = ''
-        this.charactersLeft = MAX_CSV_LENGTH
+    constructor(header) {
+        this.header = header
+        this.filesCount = 0
+        this[createNewFile]()
         this[attachEventListeners]()
     }
 
@@ -24,24 +23,12 @@ class CSVStream {
         eventEmitter.on(AGGREGATION_FINISHED, this[writeFileToDisc].bind(this));
     }
 
-    [setHeader](header) {
-        this.header = header.value
-        this.file += `${this.header}`
-        this.charactersLeft -= this.header.length
-    }
-
     [createNewFile]() {
         this.charactersLeft = (MAX_CSV_LENGTH - this.header.length)
-        this[writeFileToDisc](this.file)
         this.file = `${this.header}`
     }
 
-    [addLine](line) {
-        this.file += `\n${line.value}`
-        this.charactersLeft -= line.value.length
-    }
-
-    [writeFileToDisc](file = this.file, fileName = `aggregated${this.filesCount}`) {
+    [writeFileToDisc](file = this.file, fileName = `aggregated${++this.filesCount}`) {
         if (!fs.existsSync(WRITE_PATH)) {
             fs.mkdirSync(WRITE_PATH)
         }
@@ -57,24 +44,22 @@ class CSVStream {
             .on('finish', () => {
                 console.log(`${filePath} has been created`)
             })
-        this.filesCount++
+    }
+
+    [addLine](line) {
+        this.file += `\n${line}`
+        this.charactersLeft -= line.length
     }
 
     getTransform() {
         const self = this
         return new Transform({
-            readableObjectMode: true,
-            writableObjectMode: true,
             transform(line, encoding, cb) {
-                if (line !== null) {
-                    if (!self.header && line.type === LINE_TYPE.HEADER) {
-                        self[setHeader](line)
-                        return cb(null, line)
-                    }
-
-                    if (self.charactersLeft >= line.value.length) {
+                if (line !== '') {
+                    if (self.charactersLeft >= line.length) {
                         self[addLine](line)
                     } else {
+                        self[writeFileToDisc]()
                         self[createNewFile]()
                         self[addLine](line)
                     }
